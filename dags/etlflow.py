109bbd1d9697ef084dba98d7ac6cbf0c3b9e9167
Murat Eken <murat@meken.altostrat.com>
2024-08-02 13:50:42 +0000

    adding notebooks and dags...

---
+++ dags/etlflow.py
@@ -0,0 +1,57 @@
+import os
+
+from datetime import datetime
+
+from google.cloud.dataform_v1beta1 import WorkflowInvocation
+
+from airflow import models
+from airflow.models.baseoperator import chain
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.google.cloud.operators.dataform import (
+    DataformCancelWorkflowInvocationOperator,
+    DataformCreateCompilationResultOperator,
+    DataformCreateWorkflowInvocationOperator,
+    DataformGetCompilationResultOperator,
+    DataformGetWorkflowInvocationOperator,
+)
+from airflow.providers.google.cloud.sensors.dataform import DataformWorkflowInvocationStateSensor
+
+DAG_ID = "etl-flow"
+PROJECT_ID = os.getenv("GCP_PROJECT_ID")
+REPOSITORY_ID = os.getenv("REPOSITORY_ID")
+REGION = os.getenv("GCP_REGION")
+GIT_REF = os.getenv("GIT_REF")
+
+with models.DAG(
+    DAG_ID,
+    schedule_interval='@once', 
+    start_date=datetime(2024, 1, 1),
+    catchup=False,
+    tags=['dataform'],
+) as dag:
+
+    prep_landing_bucket_op = EmptyOperator(task_id="sources_to_landing")
+
+    df_compilation_op = DataformCreateCompilationResultOperator(
+        task_id="compile_dataform_workflow",
+        project_id=PROJECT_ID,
+        region=REGION,
+        repository_id=REPOSITORY_ID,
+        compilation_result={
+            "git_commitish": GIT_REF,
+        },
+    )
+
+    df_invocation_op = DataformCreateWorkflowInvocationOperator(
+        task_id='run_dataform_workflow',
+        project_id=PROJECT_ID,
+        region=REGION,
+        repository_id=REPOSITORY_ID,
+         workflow_invocation={
+            "compilation_result": "{{ task_instance.xcom_pull('compile_dataform_workflow')['name'] }}"
+        },
+    )
+
+    bq_inference_op = EmptyOperator(task_id="customer_churn_inferencing")
+
+prep_landing_bucket_op >> df_compilation_op >> df_invocation_op >> bq_inference_op
